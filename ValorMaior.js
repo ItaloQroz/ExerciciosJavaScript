2304cb011787f7bcdc3adb1f5093543e897c540d
Italo Queiroz <ItaloQueirozfs@gmail.com>
2022-05-20 22:14:24 -0300

    Java

--- ValorMaior.js
+++ ValorMaior.js
@@ -16,14 +16,16 @@ else if (numero3 >= numero2 && numero3 >= numero1){
     maior1 = numero3
 }
 
-if (numero1 >= numero2 && numero1 >=numero3){
+if (numero1 >= numero2 && numero1 <=numero3){
     maior2 = numero1
 }
-else if (numero2 >= numero1 && numero2 >= numero3){
+else if (numero1 <= numero2 && numero1 >= numero3){
+    maior2 = numero1
+}
+else if (numero2 >= numero1 && numero1 >= numero3){
     maior2 = numero2
 }
-else if (numero3 >= numero2 && numero3 >= numero1){
+else if (numero2 <= numero1 && numero2 >= numero3){
     maior2 = numero3
 }
-
 
