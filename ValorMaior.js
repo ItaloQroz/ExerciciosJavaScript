583c5937693c9dce761a852f5a7164c94b8a06e5
Italo Queiroz <ItaloQueirozfs@gmail.com>
2022-05-23 20:09:48 -0300

    Exercicio JavaScripto

--- ValorMaior.js
+++ ValorMaior.js
@@ -1,31 +1,42 @@
-function ValorMaior(){
- let numero1 = parseFloat(prompt( "Digite o Primeiro Número: "))
- let numero2 = parseFloat(prompt( "Digite o Segundo Número: "))
- let numero3 = parseFloat(prompt( "Digite o Terceiro Número: "))
- let maior1 = parseFloat(0);
- let maior2 = parseFloat(0);
- let valor = parseFloat(0);
+function ValorMaior() {
+    let numero1 = parseFloat(prompt("Digite o Primeiro Número: "));
+    let numero2 = parseFloat(prompt("Digite o Segundo Número: "));
+    let numero3 = parseFloat(prompt("Digite o Terceiro Número: "));
+    let maior1 = parseFloat();
+    let maior2 = parseFloat();
 
-if (numero1 >= numero2 && numero1 >=numero3){
-    maior1 = numero1
-}
-else if (numero2 >= numero1 && numero2 >= numero3){
-    maior1 = numero2
-}
-else if (numero3 >= numero2 && numero3 >= numero1){
-    maior1 = numero3
-}
 
-if (numero1 >= numero2 && numero1 <=numero3){
-    maior2 = numero1
-}
-else if (numero1 <= numero2 && numero1 >= numero3){
-    maior2 = numero1
-}
-else if (numero2 >= numero1 && numero1 >= numero3){
-    maior2 = numero2
-}
-else if (numero2 <= numero1 && numero2 >= numero3){
-    maior2 = numero3
-}
+    if (numero1 >= numero2 && numero1 >= numero3){
+        maior1 = numero1;
+    }
+    else if (numero2 >= numero1 && numero2 >= numero3){
+        maior1 = numero2;
+    }
+    
+    else if (numero3 >= numero2 && numero3>=numero1){
+        maior1 = numero3;
+    }
+    else{}
 
+    if (numero1 >= numero2 && numero1 <= numero3){
+        maior2 = numero1;
+    }
+    else if (numero1 <= numero2 && numero1 >= numero3){
+        maior2 = numero1;
+    }
+    else if (numero2 >= numero1 && numero1 <= numero3){
+        maior2 = numero2;
+    }
+    else if (numero2 <= numero1 && numero2 >= numero3){
+        maior2 = numero3;
+    }
+    else if (numero3 >= numero1 && numero3 <= numero2){
+        maior2 = numero3;
+    }
+    else if (numero3 <= numero1 && numero3 >= numero2){
+        maior2 = numero3; }
+
+
+    alert( maior1 + " - "+ maior2)
+
+}
