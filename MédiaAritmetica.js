65c03e41ea8d07deccaf81a5a24b63420dcae577
Italo Queiroz <ItaloQueirozfs@gmail.com>
2022-05-23 22:01:43 -0300

    Atualizado

--- MédiaAritmetica.js
+++ MédiaAritmetica.js
@@ -1,23 +1,27 @@
-function media(){
-    let numerototal= parseFloat(0);
-    let i =parseInt(1)
-    let n =parseFloat();
+function media() {
+  let numerototal = parseFloat(0);
+  let i = (0);
+  let n = parseInt(0);
 
-    while (i){
-    let n = parseFloat(prompt("Digite um Numero"));
-    
-       if (n % 2 == 0){
-        numerototal = numerototal + n;
-       }
 
-       if(n == 0){
-        alert("Saindo Da Sessão")   
-        break;
-           
-       }
+
+
+  while (i<=100){
+    let n = parseInt(prompt("Digite um Numero"));
     
+
+    if (n % 2 == 0) {
+      numerototal = numerototal + n;
+        i = i +1
+    }
+     if (n == 0) {
+      alert("Saindo Da Sessão");
+      break;
     }
 
-    alert("o Valor Total da soma foi = " + numerototal)
+  }
 
+  alert("o Valor A média Aritmética é = " + numerototal / (i - 1));
+  
+  console.log(i)
 }
